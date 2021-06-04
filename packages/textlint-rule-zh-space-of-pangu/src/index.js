f484a1424d909f01044bd87c9fd1927b073b2328
darkyzhou <darkyoooooo@gmail.com>
2021-06-04 23:04:48 +0800

    feat(space-of-pangu): add more rules and test cases

--- packages/textlint-rule-zh-space-of-pangu/src/index.js
+++ packages/textlint-rule-zh-space-of-pangu/src/index.js
@@ -1,38 +1,52 @@
 import { RuleHelper } from 'textlint-rule-helper';
 import { getTextContent } from 'textlint-util-zh';
 
-const WORD_REGEX = /[\w]+/gi;
+const WORD_REGEX = /[\w-]+/gi;
+const FULL_WORD_REGEX = /^[\w-]$/i;
+const SPACE_REGEX = /((?<space>[ ]+)|(?<word>[^ ]+))/g;
 
 export default (context) => {
   const { Syntax } = context;
 
   return {
     [Syntax.Code](node) {
-      doCheck(context, node, 1, Syntax);
+      doCheck(context, node, 1);
     },
     [Syntax.Str](node) {
-      doCheck(context, node, 0, Syntax);
+      doCheck(context, node, 0);
     }
   };
 };
 
-function doCheck(context, node, extraIndexPadding, syntax) {
-  const { RuleError, report } = context;
+function doCheck(context, node, extraIndexPadding) {
+  const { report } = context;
+  const errors = [
+    ...checkMissingSpaces(context, node, extraIndexPadding),
+    ...checkRedundantSpaces(context, node, extraIndexPadding)
+  ];
+
+  if (errors.length > 0) {
+    errors.forEach((error) => report(node, error));
+  }
+}
+
+function checkMissingSpaces(context, node, extraIndexPadding) {
+  const { RuleError, Syntax } = context;
   const helper = new RuleHelper(context);
   const ancestors = helper.getParents(node);
   const nodeValue = node.value;
-  const errors = [];
 
+  const errorIndexes = [];
   for (const match of nodeValue.matchAll(WORD_REGEX)) {
     const beginIndex = match.index;
     const endIndex = match.index + match[0].length - 1;
 
-    if (beginIndex > 0 && nodeValue[beginIndex - 1] !== ' ') {
-      errors.push({ index: extraIndexPadding + beginIndex });
+    if (beginIndex > 0 && !isValidSurroundingsForNonChineseWords(nodeValue[beginIndex - 1])) {
+      errorIndexes.push(beginIndex);
     }
 
-    if (endIndex < nodeValue.length - 1 && nodeValue[endIndex + 1] !== ' ') {
-      errors.push({ index: extraIndexPadding + endIndex });
+    if (endIndex < nodeValue.length - 1 && !isValidSurroundingsForNonChineseWords(nodeValue[endIndex + 1])) {
+      errorIndexes.push(endIndex);
     }
 
     let checkBeginSpace = beginIndex === 0;
@@ -53,10 +67,11 @@ function doCheck(context, node, extraIndexPadding, syntax) {
       if (checkBeginSpace) {
         if (containingNodeIndex > 0) {
           const nodeToLookForSpace = ancestorChildren[containingNodeIndex - 1];
-          if (nodeToLookForSpace.type !== syntax.Break) {
+          if (nodeToLookForSpace.type !== Syntax.Break) {
             const nodeText = getTextContent(nodeToLookForSpace);
-            if (!nodeText.endsWith(' ')) {
-              errors.push({ index: extraIndexPadding + beginIndex });
+            // TODO: handle the case when nodeText === ''
+            if (!isValidSurroundingsForNonChineseWords(nodeText.slice(-1))) {
+              errorIndexes.push(beginIndex);
             }
           }
           checkBeginSpace = false;
@@ -65,10 +80,11 @@ function doCheck(context, node, extraIndexPadding, syntax) {
       if (checkEndSpace) {
         if (containingNodeIndex < ancestorChildren.length - 1) {
           const nodeToLookForSpace = ancestorChildren[containingNodeIndex + 1];
-          if (nodeToLookForSpace.type !== syntax.Break) {
+          if (nodeToLookForSpace.type !== Syntax.Break) {
             const nodeText = getTextContent(nodeToLookForSpace);
-            if (!nodeText.startsWith(' ')) {
-              errors.push({ index: extraIndexPadding + endIndex });
+            // TODO: handle the case when nodeText === ''
+            if (!isValidSurroundingsForNonChineseWords(nodeText[0])) {
+              errorIndexes.push(endIndex);
             }
           }
           checkEndSpace = false;
@@ -77,7 +93,40 @@ function doCheck(context, node, extraIndexPadding, syntax) {
     }
   }
 
-  if (errors.length > 0) {
-    errors.forEach((error) => report(node, new RuleError('中文与数字、英文之间需要增加空格', error)));
-  }
+  return errorIndexes.map(
+    (index) => new RuleError('中文与数字或英文之间需要增加空格', { index: extraIndexPadding + index })
+  );
+}
+
+function checkRedundantSpaces(context, node, extraIndexPadding) {
+  const { RuleError, Syntax } = context;
+  const nodeValue = node.value;
+  const results = [...nodeValue.matchAll(SPACE_REGEX)];
+  const errors = [];
+  results.forEach((result, i) => {
+    const match = result[0];
+    const beginIndex = result.index;
+    const endIndex = result.index + match.length - 1;
+    const isSpace = result.groups.space;
+
+    if (isSpace) {
+      if (i < results.length - 1 && isChinesePunctuation(results[i + 1][0][0])) {
+        // TODO: no punctuations at the beginning of a line
+        errors.push({ message: '全角标点与其他字符之间不需要加空格', index: extraIndexPadding + beginIndex });
+      }
+
+      if (i > 0 && isChinesePunctuation(results[i - 1][0].slice(-1))) {
+        errors.push({ message: '全角标点与其他字符之间不需要加空格', index: extraIndexPadding + endIndex });
+      }
+    }
+  });
+  return errors.map((error) => new RuleError(error.message, { index: error.index }));
+}
+
+function isValidSurroundingsForNonChineseWords(character) {
+  return /[ ]/i.test(character) || isChinesePunctuation(character);
+}
+
+function isChinesePunctuation(word) {
+  return /^[\p{Punctuation}]+$/u.test(word) && /^[\p{Script=Han}]+$/u;
 }
--- packages/textlint-rule-zh-space-of-pangu/test/index.spec.js
+++ packages/textlint-rule-zh-space-of-pangu/test/index.spec.js
@@ -3,25 +3,45 @@ import rule from '../src/index';
 
 const tester = new TextLintTester();
 
-const MARKDOWN_VALID_CASES = ['在 LeanCloud 上，数据存储是围绕 `AVObject` 进行的。'];
-
 const PLAIN_TEXT_VALID_CASES = [
-  `测试
-Test 测试`
+  '今天出去买菜花了 5000 元。',
+  '我家的光纤入户宽带有 10Gbps，SSD 一共有 10TB。',
+  '今天是 233° 的高温。',
+  '新 MacBook Pro 有 15% 的 CPU 性能提升。',
+  '刚刚买了一部 iPhone，好开心！'
 ];
 
-const MARKDOWN_INVALID_CASES = [['在LeanCloud上，数据存储是围绕`AVObject` 进行的。', 1, 9, 20]];
+const PLAIN_TEXT_INVALID_CASES = [
+  ['今天出去买菜花了5000元。', '中文与数字或英文之间需要增加空格', 8, 11],
+  ['我家的光纤入户宽带有 10 Gbps，SSD 一共有 20 TB。', '数字与单位之间无需增加空格', 13, 29],
+  ['今天是 233 ° 的高温。', '度数、百分比与数字之间不需要增加空格', 7],
+  ['新 MacBook Pro 有 15 % 的 CPU 性能提升。', '度数、百分比与数字之间不需要增加空格', 18],
+  ['刚刚买了一部 iPhone ，好开心 ！', '全角标点与其他字符之间不需要加空格', 13, 18]
+];
+
+const MARKDOWN_VALID_CASES = [
+  '在 LeanCloud 上，数据存储是围绕 `AVObject` 进行的。每个 `AVObject` 都包含了与 JSON 兼容的 key-value 对应的数据。数据是 schema-free 的，你不需要在每个 `AVObject` 上提前指定存在哪些键，只要直接设定对应的 key-value 即可。'
+];
+
+const MARKDOWN_INVALID_CASES = [
+  ['在LeanCloud上，数据存储是围绕`AVObject` 进行的。', '中文与数字或英文之间需要增加空格', 1, 9, 20]
+];
 
 tester.run('盘古之白', rule, {
   valid: [
-    ...MARKDOWN_VALID_CASES.map((text) => ({ ext: '.md', text })),
-    ...PLAIN_TEXT_VALID_CASES.map((text) => ({ ext: '.txt', text }))
+    ...PLAIN_TEXT_VALID_CASES.map((text) => ({ ext: '.txt', text })),
+    ...MARKDOWN_VALID_CASES.map((text) => ({ ext: '.md', text }))
   ],
   invalid: [
-    ...MARKDOWN_INVALID_CASES.map((c) => ({
+    ...PLAIN_TEXT_INVALID_CASES.map((caseConfig) => ({
+      ext: '.txt',
+      text: caseConfig[0],
+      errors: caseConfig.slice(2).map((index) => ({ message: caseConfig[1], index }))
+    })),
+    ...MARKDOWN_INVALID_CASES.map((caseConfig) => ({
       ext: '.md',
-      text: c[0],
-      errors: c.slice(1).map((index) => ({ index }))
+      text: caseConfig[0],
+      errors: caseConfig.slice(2).map((index) => ({ message: caseConfig[1], index }))
     }))
   ]
 });
