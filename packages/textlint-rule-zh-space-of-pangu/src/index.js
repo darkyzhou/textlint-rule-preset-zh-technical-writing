7d03998d5eb280f7f32819d3b4bb532e18c07a6f
darkyzhou <darkyoooooo@gmail.com>
2021-06-05 23:19:47 +0800

    feat(space-of-pangu/index): configurable rules argument

--- packages/textlint-rule-zh-space-of-pangu/src/index.js
+++ packages/textlint-rule-zh-space-of-pangu/src/index.js
@@ -6,27 +6,43 @@ import { RuleHelper } from 'textlint-rule-helper';
 const TOKEN_BASED_RULE_OBJECTS = Object.values(tokenBasedRules);
 const NODE_BASED_RULE_OBJECTS = Object.values(nodeBasedRules);
 
-export default (context) => {
+export default {
+  linter: entry,
+  fixer: entry
+};
+
+function entry(context) {
   const { Syntax } = context;
+  const ruleObjects = {
+    nodeBased: NODE_BASED_RULE_OBJECTS,
+    tokenBased: TOKEN_BASED_RULE_OBJECTS
+  };
 
   return {
     [Syntax.Code](node) {
-      doCheck(context, node);
+      doCheck(ruleObjects, context, node);
     },
     [Syntax.Str](node) {
-      doCheck(context, node);
+      doCheck(ruleObjects, context, node);
     }
   };
-};
+}
 
-function doCheck(context, node) {
+export function doCheck(
+  ruleObjects = {
+    nodeBased: [],
+    tokenBased: []
+  },
+  context,
+  node
+) {
   const { report } = context;
   const value = node.value;
   const nodeTypeInLowerCase = node.type.toLowerCase();
   const helper = new RuleHelper(context);
   const errors = [];
 
-  for (const rule of NODE_BASED_RULE_OBJECTS) {
+  for (const rule of ruleObjects.nodeBased) {
     if (rule[nodeTypeInLowerCase]) {
       const error = rule[nodeTypeInLowerCase](context, node, helper);
       if (error) {
@@ -38,7 +54,7 @@ function doCheck(context, node) {
   runLexerOnString(value, ({ topToken, tokens }) => {
     const type = topToken.type;
 
-    for (const rule of TOKEN_BASED_RULE_OBJECTS) {
+    for (const rule of ruleObjects.tokenBased) {
       if (rule[type]) {
         const error = rule[type](context, node, topToken, tokens);
         if (error) {
