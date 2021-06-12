c43fe6e8010232067141608f7c446fa8efc0b215
darkyzhou <darkyoooooo@gmail.com>
2021-06-12 17:11:48 +0800

    refactor: combine space-between-zh-and-en and space-between-zh-and-num together

---
+++ packages/textlint-rule-zh-space-between-zh-and-en-or-num/src/index.js
@@ -0,0 +1,77 @@
+import {
+  getRightAdjacentNode,
+  getTextContent,
+  getLeftAdjacentNode,
+  REGEX_CHINESE_PUNCTUATION,
+  REGEX_SPACE
+} from 'textlint-util-zh';
+
+export default {
+  number: (ctx) => {
+    return check(ctx, '中文与数字之间需要添加空格');
+  },
+  en_char: (ctx) => {
+    return check(ctx, '中文与英文之间需要添加空格');
+  }
+};
+
+function check({ textLintCtx, node, currentToken, previousToken, nextToken, helper }, message) {
+  const { fixer, RuleError } = textLintCtx;
+  const errors = [];
+
+  if (previousToken?.type === 'zh_char') {
+    errors.push(
+      new RuleError(message, {
+        index: currentToken.beginIndex,
+        fix: fixer.insertTextBeforeRange([currentToken.beginIndex, currentToken.endIndex + 1], ' ')
+      })
+    );
+  }
+
+  if (nextToken?.type === 'zh_char') {
+    errors.push(
+      new RuleError(message, {
+        index: currentToken.endIndex,
+        fix: fixer.insertTextAfterRange([currentToken.beginIndex, currentToken.endIndex + 1], ' ')
+      })
+    );
+  }
+
+  const shouldCheckNode = (node) => node && ['Str', 'Delete', 'Emphasis', 'Strong', 'Link'].includes(node.type);
+
+  if (!previousToken) {
+    const leftAdjacentNode = getLeftAdjacentNode(helper, node);
+    if (shouldCheckNode(leftAdjacentNode)) {
+      const nodeTextEnd = getTextContent(leftAdjacentNode).slice(-1);
+      if (nodeTextEnd !== '\n' && !REGEX_SPACE.test(nodeTextEnd) && !REGEX_CHINESE_PUNCTUATION.test(nodeTextEnd)) {
+        errors.push(
+          new RuleError(message, {
+            index: currentToken.beginIndex,
+            fix: fixer.insertTextAfter(leftAdjacentNode, ' ')
+          })
+        );
+      }
+    }
+  }
+
+  if (!nextToken) {
+    const rightAdjacentNode = getRightAdjacentNode(helper, node);
+    if (shouldCheckNode(rightAdjacentNode)) {
+      const nodeTextBegin = getTextContent(rightAdjacentNode)[0];
+      if (
+        nodeTextBegin !== '\n' &&
+        !REGEX_SPACE.test(nodeTextBegin) &&
+        !REGEX_CHINESE_PUNCTUATION.test(nodeTextBegin)
+      ) {
+        errors.push(
+          new RuleError(message, {
+            index: currentToken.endIndex,
+            fix: fixer.insertTextBefore(rightAdjacentNode, ' ')
+          })
+        );
+      }
+    }
+  }
+
+  return errors;
+}
--- packages/textlint-rule-zh-space-between-zh-and-en-or-num/test/index.spec.js
+++ packages/textlint-rule-zh-space-between-zh-and-en-or-num/test/index.spec.js
@@ -6,7 +6,12 @@ const PLAIN_TEXT_CASES = [
   ['Test'],
   ['Test 文本', 'Test文本', '中文与英文之间需要添加空格', 3],
   ['文本 Test', '文本Test', '中文与英文之间需要添加空格', 2],
-  ['刚刚买了一部\niPhone']
+  ['刚刚买了一部\niPhone'],
+  ['今天出去买菜花了 5000 元。', '今天出去买菜花了5000元。', '中文与数字之间需要添加空格', 8, 11],
+  ['5000'],
+  ['5000 元', '5000元', '中文与数字之间需要添加空格', 3],
+  ['第 5000', '第5000', '中文与数字之间需要添加空格', 1],
+  ['5000\n元']
 ];
 
 const MARKDOWN_CASES = [
@@ -27,11 +32,12 @@ const MARKDOWN_CASES = [
     '中文与英文之间需要添加空格',
     26,
     35
-  ]
+  ],
+  ['233 [测试](https://example.com) 233', '233[测试](https://example.com)233', '中文与数字之间需要添加空格', 2, 28]
 ];
 
 runRuleTest({
-  name: 'space-between-zh-and-en',
+  name: 'space-between-zh-and-en-or-num',
   rules: [ruleObject],
   plainTextFixableCases: PLAIN_TEXT_CASES,
   markdownFixableCases: MARKDOWN_CASES
